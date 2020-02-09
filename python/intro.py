641ceae7cb562f05d8b961282128a6381059fdee
subhan <subhan.nooriansyah@gmail.com>
2020-02-09 22:26:40 +0700

    add python

---
+++ python/intro.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+
+    print("Example using variable in c \n")
+ 
+    s = int(input("Enter length of side : "))
+    print("the formula a = s * s")
+    a = s * s # calculate area; then store in a and also are variable
+
+    print("\n")
+
+    print("Area of Square = {}".format(a))
