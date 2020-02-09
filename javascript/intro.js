a294d5c735dbaaf53f0ab094bd50b393a2bb4516
subhan <subhan.nooriansyah@gmail.com>
2020-02-09 23:41:10 +0700

    add javascript but cannot show in terminal using rhino

---
+++ javascript/intro.js
@@ -0,0 +1,10 @@
+var a
+
+print("Example using variable in javascript \n");
+
+var s = prompt("Enter length of side : ");   
+print("the formula a = s * s");
+a = s * s; // calculate area; then store in a
+print("\n");
+
+print("Area of Square = %d", a);
