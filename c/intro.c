7bd767a3843d51cf7bcd89e70781d03f1515e47d
subhan <subhan.nooriansyah@gmail.com>
2020-02-09 22:37:22 +0700

    add ruby

--- c/intro.c
+++ c/intro.c
@@ -2,7 +2,7 @@
 
 int main() {
     
-    var a, s; // variable
+    int a, s; // variable
 
     printf("Example using variable in c \n");
     printf("Enter length of side : ");
---
+++ ruby/intro.rb
@@ -0,0 +1,12 @@
+
+
+puts "Example using variable in c \n"
+puts "Enter length of side : "
+
+s = gets; # store length in s
+
+puts "the formula a = s * s"
+a = (s.to_i * s.to_i) # calculate area; then store in a
+puts "\n"
+
+puts "Area of Square =", a
