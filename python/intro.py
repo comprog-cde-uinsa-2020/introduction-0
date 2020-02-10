94ac0a39f0169562110431dcdae7d515112fc92a
subhan <subhan.nooriansyah@gmail.com>
2020-02-10 21:51:42 +0700

    add branching: if elif else in python

--- python/intro.py
+++ python/intro.py
@@ -1,12 +1,24 @@
 
 if __name__ == "__main__":
 
-    print("Example using variable in c \n")
+    print("Example using variable in python \n")
  
     s = int(input("Enter length of side : "))
     print("the formula a = s * s")
-    a = s * s # calculate area; then store in a and also are variable
 
+    # variable
+    a = s * s # calculate area; then store in a and also are variable
+    
+    # branching => if elif else
+    threshold = 100
+    
+    if (s > 100):
+      print("size: {} more than threshold: {}".format(s, threshold))
+    elif( s == 100):
+      print("size: {} equal with threshold: {}".format(s, threshold))
+    else:
+      print("size: {} less than threshold: {}".format(s, threshold))
+    
     print("\n")
 
     print("Area of Square = {}".format(a))
