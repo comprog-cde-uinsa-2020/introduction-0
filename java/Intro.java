1727c00fe3e4baa2101ac364f8dad39568021472
subhan <subhan.nooriansyah@gmail.com>
2020-02-10 23:21:53 +0700

    add java languange

--- java/Intro.java
+++ java/Intro.java
@@ -1,15 +1,13 @@
-package java;
-
 import java.util.Scanner;
 
 public class Intro {
 
     public static void main(String []args) {
         
-        int a;
+        int a; // variable
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Example using variable in c \n");
+        System.out.print("Example using variable in java \n");
         System.out.print("Enter length of side : ");
  
         int s = scanner.nextInt();
@@ -18,7 +16,20 @@ public class Intro {
 
         System.out.print("the formula a = s * s");
         System.out.print("\n");
-        
+
+        int threshold = 100;
+
+        // branching 
+        System.out.print("\n");
+        if (s > threshold) {
+          System.out.print(String.format("size: %d more than threshold: %d", s, threshold));
+        } else if ( s == 100) {
+          System.out.print(String.format("size: %d equal with threshold: %d", s, threshold));
+        } else {
+          System.out.print(String.format("size: %d less than threshold: %d", s, threshold));       
+        }
+
+        System.out.print("\n");
         System.out.print(String.format("Area of Square = %d", a));
 
     }
