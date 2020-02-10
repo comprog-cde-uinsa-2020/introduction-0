2e139c79aab0691d1982926c1e9a6a96c99b13c1
subhan <subhan.nooriansyah@gmail.com>
2020-02-11 00:06:12 +0700

    update ruby

--- ruby/intro.rb
+++ ruby/intro.rb
@@ -1,10 +1,27 @@
-puts "Example using variable in c \n"
+def rectangle(s)
+    return (s * s)    
+end
+
+puts "Example using variable in ruby \n"
 puts "Enter length of side : "
 
-s = gets; # store length in s
+input = gets # store length in s
+s = input.to_i
 
 puts "the formula a = s * s"
-a = (s.to_i * s.to_i) # calculate area; then store in a
+
+a = rectangle(s) # calculate area; then store in a
+threshold = 100
+
+puts "\n"
+if s > threshold+1
+  puts "size: #{s} more than threshold: #{threshold}"
+elsif s == 100
+  puts "size: #{s} equal with threshold: #{threshold}"
+else 
+  puts "size: #{s} less than threshold: #{threshold}"       
+end
+
 puts "\n"
 
-puts "Area of Square =", a
+puts "Area of Square = #{a}"
