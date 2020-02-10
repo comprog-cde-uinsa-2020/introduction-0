b61bee924e664a3497552e10c20212d5efe5cd66
subhan <subhan.nooriansyah@gmail.com>
2020-02-10 23:06:43 +0700

    update again

--- python/intro.py
+++ python/intro.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # branching => if elif else
     threshold = 100
     
-    if (s > 100):
+    if (s > threshold):
       print("size: {} more than threshold: {}".format(s, threshold))
     elif( s == 100):
       print("size: {} equal with threshold: {}".format(s, threshold))
