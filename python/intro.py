75e1f5aef83ffe1b262b922fd8a46a7008cce353
subhan <subhan.nooriansyah@gmail.com>
2020-02-11 13:34:36 +0700

    add function python

--- python/intro.py
+++ python/intro.py
@@ -1,3 +1,6 @@
+# function
+def rectangle(s):
+  return s*s
 
 if __name__ == "__main__":
 
@@ -7,14 +10,14 @@ if __name__ == "__main__":
     print("the formula a = s * s")
 
     # variable
-    a = s * s # calculate area; then store in a and also are variable
+    a = rectangle(s) # calculate area; then store in a and also are variable
     
     # branching => if elif else
     threshold = 100
     
     if (s > (threshold+1)):
       print("size: {} more than threshold: {}".format(s, threshold))
-    elif( s == 100):
+    elif( s == threshold):
       print("size: {} equal with threshold: {}".format(s, threshold))
     else:
       print("size: {} less than threshold: {}".format(s, threshold))
