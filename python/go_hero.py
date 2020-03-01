f6089e582105bb9bc25bc59fd71c762095942666
subhan <subhan.nooriansyah@gmail.com>
2020-03-01 15:28:34 +0700

    update array python

---
+++ python/go_hero.py
@@ -0,0 +1,23 @@
+import hero
+
+
+if __name__ == "__main__":
+
+   listName = ["Saras 008", "Mr Black", "Panji Manusia Milenium", "Gatot Kaca"]
+   listPower = [100, 120, 130, 1000]
+
+   listHero = []
+
+   for i in range(len(listPower)):
+       listHero.append(hero.Hero(listName[i], listPower[i]))
+
+   print("Show all {}".format(listHero))
+
+   for j in range(len(listHero)):
+       print(" name: {} ".format(listHero[j].show_name()))
+       print(" power: {} ".format(listHero[j].show_power()))
+
+
+
+   
+
---
+++ python/hero.py
@@ -0,0 +1,11 @@
+class Hero:
+
+    def __init__(self, name, power):
+       self.name = name
+       self.power = power
+
+    def show_name(self):
+       return self.name
+
+    def show_power(self):
+       return self.power
