a65998506033511077e06e72d880338a81f7e6a5
subhan <subhan.nooriansyah@gmail.com>
2020-03-01 10:29:01 +0700

    example two files

---
+++ python/function.py
@@ -0,0 +1,4 @@
+class Formula:
+
+   def add(self, a, b):
+       return a + b
---
+++ python/main.py
@@ -0,0 +1,8 @@
+import function 
+
+if __name__ == "__main__":
+
+ a = 1
+ b = 2
+
+ print("test calling function {}".format(function.Formula().add(a, b)))
