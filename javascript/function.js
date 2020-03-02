46fa5eded387c9352820f304e44ad880522b5811
subhan <subhan.nooriansyah@gmail.com>
2020-03-02 23:23:50 +0700

    update all coding: c++ and javascript for multi files

--- javascript/function.js
+++
@@ -1,9 +0,0 @@
-// example javascript ES6
-//export function thisFunctionAdd(a, b) {
-//    return a + b;
-//}
-
-// using nodejs
-exports.thisFunctionAdd = function(a, b) {
-   return a + b;
-}
---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+console.log("test");
+
+function Hitung(dat1,dat2,dat3,dat4){
+   var total = dat1 + dat2 + dat3 + dat4;
+
+   var totalrat = total / 4;
+
+   return totalrat;
+}
+
+function InputData(Bilangan){
+   var data = parseFloat(document.getElementById(Bilangan).value);
+
+   return data;
+}
+
+function Output(data){
+   document.getElementById("rata_rata").innerHTML = data;
+}
+
+function Ratarata(){
+
+   //Input
+   var dat1 = InputData("indo");
+   var dat2 = InputData("ing");
+   var dat3 = InputData("mat");
+   var dat4 = InputData("ipa");
+
+   //Proses
+   var ratarata = Hitung(dat1,dat2,dat3,dat4);
+
+   console.log(ratarata);
+
+   //OutPut
+   Output(ratarata);
+}
